b95952e0192c9c712f54a21a04d0fc72be39f984
francisco gerardo neri andriano <fneri@nearsoft.com>
2019-11-04 20:53:46 -0600

    adding myIp

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 const express = require('express');
-
+const myIP = require('my-ip')
 const app = express();
 const port = 3000
 
 
 
-app.get('/', (req, res) => res.send(`My ec2 server works! ${req.hostname}`))
+app.get('/', (req, res) => res.send(`My ec2 server works! ${myIP()} -${ myIP(null,true)}`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
