bc2b0eae95d06810bfd692f865fddd47e6376de6
francisco gerardo neri andriano <fneri@nearsoft.com>
2019-11-04 22:12:52 -0600

    delete myIp

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 
 const express = require('express');
-const myIP = require('my-ip')
 const app = express();
 const port = 3000
 
 
 
-app.get('/', (req, res) => res.send(`My ec2 server works! ${myIP()} -${ myIP(null,true)}`))
+app.get('/', (req, res) => res.send(`My ec2 server works!`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
